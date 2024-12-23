d19f82a8d4e353117dd1747b6aacae3e1d341672
kimla1234 <chhoeurn.kimla.1221@rupp.edu.kh>
2024-12-23 08:40:04 +0700

    test deploy

--- app/(public_user)/layout.tsx
+++ app/(public_user)/layout.tsx
@@ -4,6 +4,30 @@ import { Inter, Suwannaphum } from "next/font/google";
 import NavbarPage from "@/components/Navbar/NavbarPage";
 import FooterPage from "@/components/Footer/FooterPage";
 import FloatingButtons from "@/components/General/FloatingButton";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - NormPlov", // Using the template to append the title dynamically
+    default: "NormPlov", // Default title when no page-specific title is set
+  },
+  description: "NormPlov: Find your perfect major and confidence career.",
+  openGraph: {
+    title: {
+      template: "%s - NormPlov",
+      default: "NormPlov",
+    },
+    description: "NormPlov: Find your perfect major and confidence career.",
+    images: ["https://normplov-api.shinoshike.studio/assets/metadata.png"],
+    url: "https://normplov.shinoshike.studio",
+  },
+  icons: {
+    icon: "/assets/logo.jpg", // Logo for favicon (replace with your actual logo path)
+    apple: "/assets/logo.jpg", // Apple touch icon (iOS)
+    shortcut: "/assets/logo.jpg", // Shortcut icon for browsers
+  },
+};
 
 const suwannaphum = Suwannaphum({
   subsets: ["khmer"],
--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -6,6 +6,29 @@ import FooterPage from "@/components/Footer/FooterPage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import FloatingButtons from "@/components/General/FloatingButton";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - NormPlov", // Using the template to append the title dynamically
+    default: "NormPlov", // Default title when no page-specific title is set
+  },
+  description: "NormPlov: Find your perfect major and confidence career.",
+  openGraph: {
+    title: {
+      template: "%s - NormPlov",
+      default: "NormPlov",
+    },
+    description: "NormPlov: Find your perfect major and confidence career.",
+    images: ["https://normplov-api.shinoshike.studio/assets/metadata.png"],
+    url: "https://normplov.shinoshike.studio",
+  },
+  icons: {
+    icon: "/assets/logo.jpg", // Logo for favicon (replace with your actual logo path)
+    apple: "/assets/logo.jpg", // Apple touch icon (iOS)
+    shortcut: "/assets/logo.jpg", // Shortcut icon for browsers
+  },
+};
 
 const suwannaphum = Suwannaphum({
   subsets: ["khmer"],
@@ -16,6 +39,7 @@ const inter = Inter({
   subsets: ["latin"],
   weight: ["400", "700"],
 });
+
 
 
 export default function UserLayout({
