2bed0649aa4689aa3fa22af641d80ff060e84f97
YengSokroza <yengsokroza@gmail.com>
2025-01-27 15:25:00 +0700

    add seo in main quiz

--- app/[locale]/(user)/test/page.tsx
+++ app/[locale]/(user)/test/page.tsx
@@ -16,7 +16,20 @@ export const metadata = {
     "skills test",
     "Career test",
     "Career Quiz",
-    "guide"
+    "guide",
+    "holland Code",
+    "RAISEC",
+    "MBTI",
+    "16 Personality",
+    "នាំផ្លូវ",
+    "តេស្តអាជីពការងារ",
+    "តេស្តបុគ្គលិកលក្ខណៈ",
+    "តេស្តវាយតម្លៃតាម​ចំណាប់អារម្មណ៍",
+    "តេស្តវាយតម្លៃតាមភាពខ្លាំងខ្សោយ",
+    "តេស្តវាយតម្លៃតាមរបៀបសិក្សា",
+    "តេស្តវាយតម្លៃតាមគុណតម្លៃ",
+    "តេស្តគ្រប់ការវាយតម្លៃទាំងអស់",
+    "ជំនាញ"
   ],
   openGraph: {
     title: "Quiz Page | NormPlov",
@@ -24,9 +37,9 @@ export const metadata = {
       "Discover your ideal career path through engaging assessments at NormPlov, including Personality, Interest, Value, Learning Style, and Skill test.",
     images: [
       {
-        url: "/Quiz/stepup.png",
-        width: 1000,
-        height: 1000,
+        url: "/assets/quiztemplate.png",
+        width: 1200,
+        height: 630,
         alt: "NormPlov Quiz Thumbnail",
       },
     ],
@@ -36,7 +49,7 @@ export const metadata = {
     title: "Quiz Page | NormPlov",
     description:
       "Explore Personality, Interest, Value, Learning Style, and Skill assessments at NormPlov to uncover your potential!",
-    images: ["/Quiz/stepup.png"],
+    images: ["/assets/quiztemplate.png"],
   },
 };
 
